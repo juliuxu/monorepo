2fa1fcf734ed71ca8eae42f60824790ead2cd4fe
Julian Jark <juliuxu@gmail.com>
2023-06-04 01:21:12 +0200

    update types

--- packages/notion-utils/CHANGELOG.md
+++ packages/notion-utils/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @vargtech/notion-utils
 
+## 0.0.3
+
+### Patch Changes
+
+- update types
+
 ## 0.0.2
 
 ### Patch Changes
--- packages/notion-utils/src/helpers.ts
+++ packages/notion-utils/src/helpers.ts
@@ -1,4 +1,5 @@
 import type {
+  BlockObjectResponse,
   DatabaseObjectResponse,
   ListBlockChildrenResponse,
   PageObjectResponse,
@@ -174,37 +175,20 @@ export const getDatabasePropertySelectOptions = (
   return undefined;
 };
 
-// Some typescript magic to extract the correct type
-type MaybeBlockResponse = ListBlockChildrenResponse["results"][number];
-const assertBlockObjectResponse = (block: MaybeBlockResponse) => {
-  if ("type" in block) return block;
-  throw new Error("passed block is not a BlockObjectResponse");
-};
-export type Block = ReturnType<typeof assertBlockObjectResponse>;
-export type BlockType = Block["type"];
-
-export type BlockWithChildren = Block & {
-  children?: BlockWithChildren;
-};
-
-export type RichText = Extract<Block, { type: "heading_1" }>["heading_1"];
-
+// Some typescript magic to extract the correct types
+export type RichText = Extract<
+  BlockObjectResponse,
+  { type: "heading_1" }
+>["heading_1"];
 export type RichTextItem = Extract<
-  Block,
+  BlockObjectResponse,
   { type: "paragraph" }
 >["paragraph"]["rich_text"][number];
 
 export type RichTextColor = RichTextItem["annotations"]["color"];
-
-// TODO: Infer from @notionhq/client
-export type SelectColor =
-  | "default"
-  | "gray"
-  | "brown"
-  | "orange"
-  | "yellow"
-  | "green"
-  | "blue"
-  | "purple"
-  | "pink"
-  | "red";
+export type SelectColor = NonNullable<
+  Extract<
+    PageObjectResponse["properties"][string],
+    { type: "select" }
+  >["select"]
+>["color"];
