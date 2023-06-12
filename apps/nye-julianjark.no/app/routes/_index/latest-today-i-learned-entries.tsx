a2780465494b488ac62affae81bb9d7c4dd29213
Julian Jark <juliuxu@gmail.com>
2023-06-12 23:58:04 +0200

    :lipstick: style cards

--- apps/nye-julianjark.no/app/routes/_index/latest-today-i-learned-entries.tsx
+++ apps/nye-julianjark.no/app/routes/_index/latest-today-i-learned-entries.tsx
@@ -1,6 +1,7 @@
 import { slugify } from "@julianjark/notion-utils";
 import { Link, useLoaderData } from "@remix-run/react";
 import type { loader } from "~/routes/_index/route";
+import { classes } from "../$notionPage/notion-driven-page";
 
 const dateFormatter = new Intl.DateTimeFormat("nb-NO", {
   day: "numeric",
@@ -10,24 +11,23 @@ const dateFormatter = new Intl.DateTimeFormat("nb-NO", {
 export function LatestTodayILearnedEntries() {
   const { latestTodayILearnedEntries } = useLoaderData<typeof loader>();
   return (
-    <ul className="mt-4 grid gap-x-[5vw] gap-y-4 sm:grid-cols-2 md:grid-cols-3">
+    <ul className="mt-4 grid gap-x-[5vw] gap-y-8 sm:grid-cols-2 md:grid-cols-3">
       {latestTodayILearnedEntries.map((entry) => (
         <li key={entry.id}>
-          <article className="card bg-base-100 shadow-xl">
-            <div className="card-body">
-              <time className="text-lg">
-                {dateFormatter.format(new Date(entry.created))}
-              </time>
-              <h3 className="card-title text-xl">
-                <Link
-                  prefetch="intent"
-                  to={`/i-dag-lærte-jeg/${slugify(entry.title)}`}
-                >
-                  {entry.title}
-                </Link>
-              </h3>
-              <p className="text-lg">{entry.summary}</p>
-            </div>
+          <article>
+            <time className="text-lg sm:text-xl md:text-2xl">
+              {dateFormatter.format(new Date(entry.created))}
+            </time>
+            <h3 className="">
+              <Link
+                className={classes.rich_text_anchor}
+                prefetch="intent"
+                to={`/i-dag-lærte-jeg/${slugify(entry.title)}`}
+              >
+                {entry.title}
+              </Link>
+            </h3>
+            <p className="mt-3 text-lg">{entry.summary}</p>
           </article>
         </li>
       ))}
