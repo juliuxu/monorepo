64e16f672046c60dbe0153808bbcf2a408dc11d0
Julian Jark <juliuxu@gmail.com>
2023-06-14 23:55:28 +0200

    :construction: start on preview mode

--- apps/nye-julianjark.no/app/config.server.ts
+++ apps/nye-julianjark.no/app/config.server.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 // ENV
 const envVariables = z.object({
   NOTION_TOKEN: z.string().nonempty(),
+  PREVIEW_SECRET: z.string().nonempty(),
 });
 envVariables.parse(process.env);
 
@@ -17,6 +18,8 @@ const YEAR_IN_SECONDS = 60 * 60 * 24 * 365;
 export const config = {
   // eslint-disable-next-line turbo/no-undeclared-env-vars
   notionToken: process.env.NOTION_TOKEN ?? "",
+  // eslint-disable-next-line turbo/no-undeclared-env-vars
+  previewSecret: process.env.PREVIEW_SECRET ?? "",
 
   landingPageId: "eb763c82093c48d1955de8cc4ea450cb",
   notionDrivenPagesDatabaseId: "f61d11c80e4b40e2a4329cde350bb31a",
