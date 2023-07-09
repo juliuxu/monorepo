c3963aec30c5ac47030be56d2872e173c1645743
Julian Jark <juliuxu@gmail.com>
2023-07-09 23:53:52 +0200

    fix: manually handle svg's

--- apps/nye-julianjark.no/app/routes/api.optimized-image.$.ts
+++ apps/nye-julianjark.no/app/routes/api.optimized-image.$.ts
@@ -1,14 +1,31 @@
 import type { LoaderArgs } from "@remix-run/node";
 import { createIPX, handleRequest } from "ipx";
 
+const domains = ["nye.julianjark.no", "picsum.photos"];
 const ipx = createIPX({
-  domains: ["nye.julianjark.no", "picsum.photos"],
+  domains,
 });
 
 export const loader = async ({ params }: LoaderArgs) => {
   let ipxPath = params["*"];
   if (!ipxPath) throw new Error("No path provided");
   ipxPath = "/" + ipxPath;
+
+  // Hack: IPX doesn't allow to never convert svg's
+  // So we need to do a initial fetch to see if it's a svg and return it as is if so
+  // Since this endpoint is cached by nginx the cost of this extra hack is fine
+  const url = new URL(ipxPath.split("/").slice(2).join("/"));
+  if (!domains.includes(url.hostname)) throw new Error("Invalid hostname");
+  const imageResponse = await fetch(url);
+  if (imageResponse.headers.get("content-type")?.includes("svg")) {
+    return new Response(await imageResponse.body, {
+      headers: {
+        "content-type": "image/svg+xml",
+      },
+      status: imageResponse.status,
+      statusText: imageResponse.statusText,
+    });
+  }
 
   const ipxResponse = await handleRequest(
     {
@@ -17,11 +34,9 @@ export const loader = async ({ params }: LoaderArgs) => {
     ipx
   );
 
-  const response = new Response(ipxResponse.body, {
+  return new Response(ipxResponse.body, {
     headers: ipxResponse.headers,
     status: ipxResponse.statusCode,
     statusText: ipxResponse.statusMessage,
   });
-
-  return response;
 };
