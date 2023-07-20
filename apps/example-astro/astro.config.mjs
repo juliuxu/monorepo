646c95575aaa19150a0274827a8aa898f5f16803
Julian Jark <juliuxu@gmail.com>
2023-07-20 21:45:20 +0200

    :rocket: add cloudflare

--- apps/example-astro/astro.config.mjs
+++ apps/example-astro/astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
-import node from "@astrojs/node";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: cloudflare(),
 });
