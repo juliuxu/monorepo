ca1999b716d7cc0608744e2157a96f8353213a8f
Julian Jark <juliuxu@gmail.com>
2023-07-29 19:04:45 +0200

    :fire: remove commented code

--- apps/nye-julianjark.no/app/routes/api.preview-mode/route.tsx
+++ apps/nye-julianjark.no/app/routes/api.preview-mode/route.tsx
@@ -26,38 +26,3 @@ export function PreviewModeToggle() {
     />
   );
 }
-
-// export function PreviewModeToggle2() {
-//   const { previewMode } = useRouteLoaderData("root") as SerializeFrom<
-//     typeof loader
-//   >;
-//   async function togglePreviewMode() {
-//     const secret =
-//       previewMode?.secret ?? window.prompt("Fyll inn preview secret") ?? "";
-//     const previewModeToSend: PreviewMode = {
-//       enabled: !previewMode?.enabled,
-//       secret,
-//     };
-//     const response = await fetch(`/api/preview-mode`, {
-//       method: "POST",
-//       body: JSON.stringify(previewModeToSend),
-//       headers: { "Content-Type": "application/json" },
-//     });
-//     if (response.ok) {
-//       window.location.reload();
-//     }
-//   }
-//   useShortcut("pp", togglePreviewMode);
-//   if (!previewMode) return null;
-//   if (!previewMode.enabled) return null;
-//   return (
-//     <div
-//       lang="en"
-//       className="fixed right-0 top-0 rounded-bl-lg border-secondary bg-secondary/20 p-2 text-secondary backdrop-blur"
-//     >
-//       <button onClick={togglePreviewMode} title="Toggle preview mode">
-//         👀 Preview
-//       </button>
-//     </div>
-//   );
-// }
