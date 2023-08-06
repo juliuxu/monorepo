2f0e2c20b45f4515938e3b02a30d441dcd989642
Julian Jark <juliuxu@gmail.com>
2023-08-06 15:38:47 +0200

    update script

--- apps/nye-julianjark.no/copy-vercel-og-files-to-build.sh
+++ apps/nye-julianjark.no/copy-vercel-og-files-to-build.sh
@@ -1,5 +1,11 @@
 # Files needed for @vercel/og to work
 mkdir -p ./build
-cp ../../node_modules/@vercel/og/dist/noto-sans-v27-latin-regular.ttf ./build/
-cp ../../node_modules/@vercel/og/dist/yoga.wasm ./build/
-cp ../../node_modules/@vercel/og/dist/resvg.wasm ./build/
+if [ -d "./node_modules/@vercel/og/" ]; then
+  cp ./node_modules/@vercel/og/dist/noto-sans-v27-latin-regular.ttf ./build/
+  cp ./node_modules/@vercel/og/dist/yoga.wasm ./build/
+  cp ./node_modules/@vercel/og/dist/resvg.wasm ./build/
+else
+  cp ../../node_modules/@vercel/og/dist/noto-sans-v27-latin-regular.ttf ./build/
+  cp ../../node_modules/@vercel/og/dist/yoga.wasm ./build/
+  cp ../../node_modules/@vercel/og/dist/resvg.wasm ./build/
+fi
