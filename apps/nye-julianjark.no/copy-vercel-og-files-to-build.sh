e7d37dcc9b85794037cafe22cebc835ec13bb952
Julian Jark <juliuxu@gmail.com>
2023-08-06 15:15:51 +0200

    manually copy required files for @vercel/og to build folder

---
+++ apps/nye-julianjark.no/copy-vercel-og-files-to-build.sh
@@ -0,0 +1,5 @@
+# Files needed for @vercel/og to work
+mkdir -p ./build
+cp ../../node_modules/@vercel/og/dist/noto-sans-v27-latin-regular.ttf ./build/
+cp ../../node_modules/@vercel/og/dist/yoga.wasm ./build/
+cp ../../node_modules/@vercel/og/dist/resvg.wasm ./build/
