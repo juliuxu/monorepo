642a66306518d2eb5dbebd180c156ed236019321
Julian Jark <juliuxu@gmail.com>
2023-09-03 16:16:59 +0200

    :white_check_mark: test social image

--- apps/nye-julianjark.no/tests/i-dag-lærte-jeg.spec.ts
+++ apps/nye-julianjark.no/tests/i-dag-lærte-jeg.spec.ts
@@ -22,3 +22,26 @@ test("i dag lærte jeg", async ({ page }) => {
   );
   await expect(page).toHaveTitle(new RegExp(entryTitle));
 });
+
+test("i dag lærte jeg - social image", async ({ page }) => {
+  await page.goto("/i-dag-lærte-jeg");
+
+  // Navigate to the first entry
+  await page
+    .getByTestId("i-dag-lærte-jeg-entries")
+    .getByRole("listitem")
+    .first()
+    .getByRole("link")
+    .click();
+
+  const socialImageUrl = await page
+    .locator("meta[property='og:image']")
+    .first()
+    .getAttribute("content");
+  expect(socialImageUrl).not.toBeNull();
+
+  const imageResponse = await fetch(socialImageUrl!);
+  expect(imageResponse.ok).toBe(true);
+  expect(imageResponse.headers.get("content-type")).toBe("image/png");
+  expect((await imageResponse.blob()).size).toBeGreaterThan(0);
+});
