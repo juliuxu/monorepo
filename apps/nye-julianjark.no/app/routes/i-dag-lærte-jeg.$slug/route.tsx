f5aea1902c33c4848f8acad5df1d0bebfc7998e3
Julian Jark <juliuxu@gmail.com>
2023-09-03 16:18:04 +0200

    bump og version
    ngnix caches the images

--- apps/nye-julianjark.no/app/routes/i-dag-lærte-jeg.$slug/route.tsx
+++ apps/nye-julianjark.no/app/routes/i-dag-lærte-jeg.$slug/route.tsx
@@ -26,7 +26,7 @@ export const meta: MetaFunction<typeof loader> = ({ data }) => {
     { property: "twitter:title", content: data?.entry.title },
     {
       property: "og:image",
-      content: `${data?.basePath}/i-dag-lærte-jeg/${data?.entry.slug}/og?v=0.0.1`,
+      content: `${data?.basePath}/i-dag-lærte-jeg/${data?.entry.slug}/og?v=0.1.0`,
     },
     { property: "og:type", content: "summary_large_image" },
     { property: "twitter:card", content: "summary_large_image" },
